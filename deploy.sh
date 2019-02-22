3249b15de716fa10db663ada06d4f4481caac9f7
jasonchi38 <jasonchi@email.com>
2019-02-22 10:25:51 -0500

    change docker hub id

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t jasonchi38/multi-client:latest -t jasonchi38/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t jasonchi38/multi-server:latest -t jasonchi38/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t jasonchi38/multi-worker:latest -t jasonchi38/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push jasonchi38/multi-client:latest
-docker push jasonchi38/multi-server:latest
-docker push jasonchi38/multi-worker:latest
-docker push jasonchi38/multi-client:$SHA
-docker push jasonchi38/multi-server:$SHA
-docker push jasonchi38/multi-worker:$SHA
+docker build -t jasonchi/multi-client:latest -t jasonchi/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jasonchi/multi-server:latest -t jasonchi/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jasonchi/multi-worker:latest -t jasonchi/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push jasonchi/multi-client:latest
+docker push jasonchi/multi-server:latest
+docker push jasonchi/multi-worker:latest
+docker push jasonchi/multi-client:$SHA
+docker push jasonchi/multi-server:$SHA
+docker push jasonchi/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=jasonchi38/multi-server:$SHA
-kubectl set image deployemnts/client-deployment client=jasonchi38/multi-client:$SHA
-kubectl set image deployemnts/worker-deployemnt worker=jasonchi38/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=jasonchi/multi-server:$SHA
+kubectl set image deployemnts/client-deployment client=jasonchi/multi-client:$SHA
+kubectl set image deployemnts/worker-deployemnt worker=jasonchi/multi-worker:$SHA
